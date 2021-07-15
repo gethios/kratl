7e1282c8743f4352ecfc99196325c297b72a740c
Joacim Høyland Eckholdt <joacimeckholdt@gmail.com>
2021-07-15 21:01:12 +0200

    feat(timer): Only fetch images between 6 AM and 7 PM

--- app.ts
+++ app.ts
@@ -1,5 +1,10 @@
 ﻿const IMG_URL = "https://www.fjordbat.no/webcam/mf_kragero.jpg";
 const FETCH_INTERVAL = 45000;
+const HOURS_DAY = 24
+const START_HOUR = 6
+const SECONDS_HOUR = 3600
+const MILLISECOND_SECOND = 1000;
+const END_HOUR = 19;
 
 function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -17,7 +22,20 @@ async function fetchImage(): Promise<void> {
 
 const app = async () => {
   console.log(`Start fetching images from ${IMG_URL}`);
+
+  const currentHour = new Date().getHours();
+
+  if(currentHour >= END_HOUR) {
+    const sleepTime = (HOURS_DAY - currentHour) + START_HOUR;
+    console.log(`Sleeping for ${sleepTime} hours`);
+    await sleep(sleepTime * SECONDS_HOUR * MILLISECOND_SECOND);
+  }
+
   while (true) {
+      if(new Date().getHours() >= END_HOUR) {
+          break;
+      }
+
       await fetchImage();
       await sleep(FETCH_INTERVAL);
   }

7e2ace1f4d3a7ebcbf9e788e04c619a97125986b
Joacim Høyland Eckholdt <joacimeckholdt@gmail.com>
2021-07-15 21:11:46 +0200

    chore(fmt): Fixed formatting with deno fmt

--- app.ts
+++ app.ts
@@ -1,14 +1,14 @@
 ﻿const IMG_URL = "https://www.fjordbat.no/webcam/mf_kragero.jpg";
 const FETCH_INTERVAL = 45000;
-const HOURS_DAY = 24
-const START_HOUR = 6
-const SECONDS_HOUR = 3600
+const HOURS_DAY = 24;
+const START_HOUR = 6;
+const SECONDS_HOUR = 3600;
 const MILLISECOND_SECOND = 1000;
 const END_HOUR = 19;
 
 function sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 async function fetchImage(): Promise<void> {
   const img = await fetch(IMG_URL);
@@ -25,19 +25,19 @@ const app = async () => {
 
   const currentHour = new Date().getHours();
 
-  if(currentHour >= END_HOUR) {
+  if (currentHour >= END_HOUR) {
     const sleepTime = (HOURS_DAY - currentHour) + START_HOUR;
     console.log(`Sleeping for ${sleepTime} hours`);
     await sleep(sleepTime * SECONDS_HOUR * MILLISECOND_SECOND);
   }
 
   while (true) {
-      if(new Date().getHours() >= END_HOUR) {
-          break;
-      }
+    if (new Date().getHours() >= END_HOUR) {
+      break;
+    }
 
-      await fetchImage();
-      await sleep(FETCH_INTERVAL);
+    await fetchImage();
+    await sleep(FETCH_INTERVAL);
   }
 };
 

79074d25065a6c4cbfb50a3de04ad8bb6813295d
Joacim Høyland Eckholdt <joacimeckholdt@gmail.com>
2021-07-16 13:57:58 +0200

    fix(fetch): Retry fetching if it fails

--- app.ts
+++ app.ts
@@ -1,22 +1,28 @@
 ﻿const IMG_URL = "https://www.fjordbat.no/webcam/mf_kragero.jpg";
 const FETCH_INTERVAL = 45000;
+
 const HOURS_DAY = 24;
-const START_HOUR = 6;
 const SECONDS_HOUR = 3600;
 const MILLISECOND_SECOND = 1000;
-const END_HOUR = 19;
+
+const START_HOUR = 6;
+const END_HOUR = 22;
 
 function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function fetchImage(sequenceNumber: number): Promise<void> {
+  const num = `${sequenceNumber}`.padStart(4, "0");
+  const date = new Date();
+  console.log(
+    `[${date.toLocaleDateString()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}]: Fetched new image #${num}`,
+  );
+
   const img = await fetch(IMG_URL);
 
-  console.log(`Fetched new image #${sequenceNumber}`);
-
   const imageBytes = new Uint8Array(await img.arrayBuffer());
-  await Deno.writeFile(`./images/image${sequenceNumber}.jpg`, imageBytes);
+  await Deno.writeFile(`./images/image${num}.jpg`, imageBytes);
 }
 
 const app = async () => {
@@ -24,8 +30,15 @@ const app = async () => {
 
   const currentHour = new Date().getHours();
 
+  let sleepTime = 0;
+
   if (currentHour >= END_HOUR) {
-    const sleepTime = (HOURS_DAY - currentHour) + START_HOUR;
+    sleepTime = (HOURS_DAY - currentHour) + START_HOUR;
+  } else if (currentHour < START_HOUR) {
+    sleepTime = START_HOUR - currentHour;
+  }
+
+  if (sleepTime != 0) {
     console.log(`Sleeping for ${sleepTime} hours`);
     await sleep(sleepTime * SECONDS_HOUR * MILLISECOND_SECOND);
   }
@@ -36,9 +49,14 @@ const app = async () => {
       break;
     }
 
-    await fetchImage(sequenceNumber);
-    sequenceNumber += 1;
+    try {
+      await fetchImage(sequenceNumber);
+    } catch (error) {
+      console.dir(error);
+      continue;
+    }
 
+    sequenceNumber += 1;
     await sleep(FETCH_INTERVAL);
   }
 };
